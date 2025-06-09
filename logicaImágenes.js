c7cfca4bfaf68f119766e47a3ff5e05121fe9421
Eduardo Del Aguila <eduardodelaguila10@gmail.com>
2025-06-09 00:18:02 -0500

    Ecualizacion Añadida

--- logicaImágenes.js
+++ logicaImágenes.js
@@ -17,67 +17,47 @@ const ctxHist = canvasHist.getContext("2d")
 const Ecualizar = document.querySelector("#Ecualizar")
 const Expandir = document.querySelector("#Expandir");
 
-let imagenRGB = [];
 
 
 
+//Ecualizacion
 Ecualizar.addEventListener("click", () => {
     handleAction('ecualizar');
     // Imagen
-    const canvasEcualizar = document.querySelector("#ecualizacion");
-    const ctxEcualizar = canvasEcualizar.getContext("2d")
-    const canvasHistEcualizado = document.querySelector("#histogramaEcualizado");
-    const ctxHistEcualizado = canvasHistEcualizado.getContext("2d");
+    const canvasEcualizar = document.getElementById('ecualizacion');
+    const ctxEcualizar = canvasEcualizar.getContext('2d')
+
+    const canvasHistEcualizado = document.getElementById('histogramaEcualizado');
+    const ctxHistEcualizado = canvasHistEcualizado.getContext('2d');
     // Obtener los datos de la imagen original
     canvasEcualizar.width = img.width;
     canvasEcualizar.height = img.height;
     canvasHistEcualizado.width = 256;
     canvasHistEcualizado.height = 200;
+
+    const resultado = Ecualización(imagenRGB)
+    const histogramaEcualizado = calcularHistograma(resultado)
+
+    dibujarHistograma(histogramaEcualizado,canvasHistEcualizado,ctxHistEcualizado)
     
-    const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const imgData = ctxEcualizar.createImageData(canvasEcualizar.width,canvasEcualizar.height)
+    const data = imgData.data
+
+    for (let i = 0; i <resultado.length; i++) {
+        const gris =resultado[i];
+        const j    = i * 4;
+        data[j] = gris;  
+        data[j + 1] = gris;  
+        data[j + 2] = gris;  
+        data[j + 3] = 255;   
+    }
+
     ctxEcualizar.putImageData(imgData, 0, 0);
-
-    const histograma = calcularHistograma(convertirABlancoYNegro(imgData));
-    const cdf = calcularCDF(histograma);
-    //verificar bien la nueva imagen
-    const imagenEcualizada = ecualizarImagen(imagenRGB, cdf);
-    const nuevoHistograma = calcularHistograma(imagenEcualizada);
-    dibujarHistograma(nuevoHistograma, canvasHistEcualizado, ctxHistEcualizado);
-    
-
 
 })
 
-//revisar mejor esta parte del codigo
 
-const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-const ecualizarHistograma = (imgData) => {
-    const histograma = calcularHistograma(convertirABlancoYNegro(imgData));
-    const cdf = new Array(256).fill(0);
-    let sum = 0;
-
-    for (let i = 0; i < histograma.length; i++) {
-        sum += histograma[i];
-        cdf[i] = sum;
-    }
-    for (let i = 0; i < cdf.length; i++) {
-        cdf[i] = Math.round((cdf[i] / sum) * 255);
-    }
-    for (let i = 0; i < imgData.data.length; i += 4) {
-        const gris = imgData.data[i]; 
-        const nuevoValor = cdf[gris];
-        imgData.data[i] = nuevoValor;
-        imgData.data[i + 1] = nuevoValor;
-        imgData.data[i + 2] = nuevoValor;
-    }
-    ctxEcualizar.putImageData(imgData, 0, 0);
-}
-
-
-
-
-
-//Expansion a futuro
+//Expanción
 Expandir.addEventListener("click", () => {
     const canvasExpandir = document.querySelector("#expancion");
     const ctxExpandir = canvasExpandir.getContext("2d");
@@ -87,6 +67,8 @@ Expandir.addEventListener("click", () => {
 
 })
 
+
+//CARGAR IMAGEN
 inputImagen.addEventListener("change", event => {
     const archivoInicial = event.target.files[0];
     lector.onload = (e) => {
@@ -94,36 +76,25 @@ inputImagen.addEventListener("change", event => {
             canvas.width = img.width;
             canvas.height = img.height;
             canvasHist.width = 256; 
-            canvasHist.height = 200; 
-
+            canvasHist.height = 200;
             ctx.drawImage(img, 0, 0);
-            
             const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-            
             const newImg = convertirABlancoYNegro(imgData);
             ctx.putImageData(imgData, 0, 0);
-            
-            imagenRGB = newImg 
-            img.data = imagenRGB
-
+            imagenRGB = newImg;
+            img.data = imagenRGB;
             const histograma = calcularHistograma(newImg);
             dibujarHistograma(histograma, canvasHist, ctxHist);
-            
         };
         img.src = e.currentTarget.result;
     };
     lector.readAsDataURL(archivoInicial);
-    
-    
-
 })
 
 
 // Convertimnos a blanco en niveles RGBA
 const convertirABlancoYNegro = (imgData) => {
-    // Entrada de  colores Rgb : imgData.data
-    miImagen = []
-
+    miImagen = [];
     for(let i = 0; i < imgData.data.length; i += 4) {
         const r = imgData.data[i];
         const g = imgData.data[i + 1];
@@ -141,29 +112,48 @@ const convertirABlancoYNegro = (imgData) => {
     return miImagen;
 }
 
-// Ecualización
-const calcularCDF = (histograma) => {
-    const cdf = new Array(256).fill(0);
-    cdf[0] = histograma[0];
-    for (let i = 1; i < 256; i++) {
-        cdf[i] = cdf[i - 1] + histograma[i];
-    }
-    return cdf;
-};
-
-const ecualizarImagen = (imagen, cdf) => {
-    const cdfMin = cdf.find(v => v > 0);
-    const totalPixeles = imagen.length;
-    return imagen.map(valor => {
-        return Math.round(((cdf[valor] - cdfMin) / (totalPixeles - cdfMin)) * 255);
-    });
-};
-
-//Futura Expancion 
-
-// ****************************/
-
 //Hitogramna Imagen Original
+const Ecualización = (newImg) => {
+    //cantidades por cada nivel
+    const cantidades = new Array(256).fill(0);
+    for (let i = 0; i < newImg.length; i++) {
+        const valor = newImg[i];
+        cantidades[valor]++;
+    }
+
+    //niveles de colores existentes
+    const unicos = [...new Set(newImg)].sort((a, b) => a - b);
+
+    const acumulado = new Array(256).fill(0);
+    let suma = 0;
+    for (let v = 0; v < 256; v++) {
+        suma += cantidades[v];
+        acumulado[v] = suma;
+    }
+
+    //definimos constantes de la formula
+    const cdfMin = acumulado.find(c => c > 0);
+    const total = newImg.length; 
+    const L = 256;              
+    const h = new Array(256).fill(0);
+
+    for (const v of unicos) {
+        const cdfV = acumulado[v];
+        //Formula del video para ecualizar(es necesaria ya que estamos trabajando en niveles de 255)
+        const nuevo = Math.round( (cdfV - cdfMin) / (total - cdfMin) * (L - 1) );
+        h[v] = nuevo;
+    }
+
+    const salida = new Uint8ClampedArray(total);
+    for (let i = 0; i < total; i++) {
+    salida[i] = h[newImg[i]];
+    }
+
+    return salida;
+}
+
+
+//Caluclo de histogramas y dibujado en el canvas
 const calcularHistograma = (newImg) => {
     const histograma = new Array(256).fill(0);
     for (let i = 0; i < newImg.length; i++) {
@@ -173,20 +163,19 @@ const calcularHistograma = (newImg) => {
     return histograma;
 }    
 
-const dibujarHistograma = (histograma, canvas, contexto) => {
-    contexto.fillStyle = "white";
-    contexto.fillRect(0, 0, canvas.width, canvas.height);
+const dibujarHistograma = (histograma, canvas, ctx) => {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     
     const max = Math.max(...histograma);
-    
     const scale = canvas.height / max
     
-    contexto.fillStyle = "black";
+    ctx.fillStyle = "black";
     const barWidth = canvas.width / 256;
     
     for (let i = 0; i < 256; i++) {
         const barHeight = histograma[i] * scale;
-        contexto.fillRect(
+        ctx.fillRect(
             i * barWidth,
             canvas.height - barHeight,
             barWidth,
@@ -195,6 +184,7 @@ const dibujarHistograma = (histograma, canvas, contexto) => {
     }
 }
 
+let imagenRGB = [];
 
 // Maneja las acciones de los botones añadiendo contenido
 function handleAction(action) {
